8e8e15f1f34e8217911ddd419e35b41268fb5c8b
vinibeloni <vinibelonicubas@gmail.com>
2025-11-19 23:15:23 -0300

    grass effect scene

--- godot-4/world/grass.gd
+++ godot-4/world/grass.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+const GrassEffect = preload("res://effects/grass_effect.tscn")
+
 @onready var area_2d: Area2D = $Area2D
 
 func _ready() -> void:
 	area_2d.area_entered.connect(_on_area_2d_area_entered)
 
 func _on_area_2d_area_entered(other_area: Area2D) -> void:
+	var grassEffect = GrassEffect.instantiate()
+	get_tree().current_scene.add_child(grassEffect)
+	grassEffect.global_position = global_position
 	queue_free()
