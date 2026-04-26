ddae2453a9a5286226e2bd4ae9833c863b86db81
vinibeloni <vinibelonicubas@gmail.com>
2026-04-26 16:00:41 -0300

    fix double shot

--- godot-4/damage_boxes/hitbox.gd
+++ godot-4/damage_boxes/hitbox.gd
@@ -3,6 +3,12 @@ class_name Hitbox extends Area2D
 @export var damage = 1
 @export var knockback_amount: = 200
 @export var knockback_direction: Vector2
+@export var store_targets: bool = false
+
+var targets: Array
 
 func hit() -> Vector2:
 	return knockback_direction * knockback_amount
+
+func clear_targets() -> void:
+	targets.clear()
--- godot-4/damage_boxes/hurtbox.gd
+++ godot-4/damage_boxes/hurtbox.gd
@@ -5,7 +5,14 @@ signal hurt(hitbox: Hitbox)
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	
-func _on_area_entered(hitbox: Area2D) -> void:
+func _on_area_entered(area2d: Area2D) -> void:
 	# print(hitbox.get_parent().name)
-	if hitbox is not Hitbox: return
+	if area2d is not Hitbox: return
+
+	# gambiarra pra previnir duplo ataque
+	var hitbox = area2d as Hitbox
+	if self in hitbox.targets: return
+	if hitbox.store_targets: hitbox.targets.append(self)
+	# -
+	
 	hurt.emit(hitbox)
